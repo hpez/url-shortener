8d5caf67fdfbc695fd73eb3e356b9cb1fd4659ab
Hassan Pezeshk <hpez58@gmail.com>
2020-09-19 13:53:04 +0430

    Avoid duplicate urls

--- src/UrlShortener.php
+++ src/UrlShortener.php
@@ -4,19 +4,26 @@
 namespace Hpez\UrlShortener;
 
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class UrlShortener
 {
     public function shorten($url)
     {
-        do {
-            $shortened = bin2hex(random_bytes(5));
-            $exists = ShortenedUrl::where('shortened', $shortened)->exists();
-        } while ($exists);
+        try {
+            $shortened = ShortenedUrl::where('url', $url)->firstOrFail();
+            $shortened = $shortened->shortened;
+        } catch (ModelNotFoundException $exception) {
+            do {
+                $shortened = bin2hex(random_bytes(5));
+                $exists = ShortenedUrl::where('shortened', $shortened)->exists();
+            } while ($exists);
 
-        ShortenedUrl::create([
-            'url' => $url,
-            'shortened' => $shortened
-        ]);
+            ShortenedUrl::create([
+                'url' => $url,
+                'shortened' => $shortened
+            ]);
+        }
 
         return $shortened;
     }
